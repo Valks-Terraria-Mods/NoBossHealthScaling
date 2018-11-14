024be2ae415bfaae465135ba8f2da53b6f13bd49
valkyrienyanko <sebastianbelle074@gmail.com>
2018-11-13 19:24:18 -0600

    commit

---
+++ BossHealthScaling.cs
@@ -0,0 +1,14 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace NoBossHealthScaling
+{
+    public class BossHealthScaling : GlobalNPC
+    {
+        public override void ScaleExpertStats(NPC npc, int numPlayers, float bossLifeScale) {
+            Main.expertMode = false;
+            npc.SetDefaults(npc.type);
+            Main.expertMode = true;
+        }
+    }
+}
---
+++ NoBossHealthScaling.cs
@@ -0,0 +1,11 @@
+using Terraria.ModLoader;
+
+namespace NoBossHealthScaling
+{
+	class NoBossHealthScaling : Mod
+	{
+		public NoBossHealthScaling()
+		{
+		}
+	}
+}
